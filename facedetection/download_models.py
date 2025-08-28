0a777a88920ebdaf592387f2303a5d6a668cb442
S.Hari Prasanth <149816654+HARI2K24-WEB@users.noreply.github.com>
2025-08-28 22:36:40 +0530

    uo

--- facedetection/download_models.py
+++ facedetection/download_models.py
@@ -1,6 +1,5 @@
 # download_models.py
 import urllib.request
-
 urls = {
     "deploy.prototxt": "https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt",
     "res10_300x300_ssd_iter_140000.caffemodel": "https://github.com/opencv/opencv_3rdparty/raw/dnn_samples_face_detector_20170830/res10_300x300_ssd_iter_140000.caffemodel",
@@ -14,4 +13,4 @@ for fname, url in urls.items():
     urllib.request.urlretrieve(url, fname)
 print("All done — models are in this folder.")
 
-
+#####
